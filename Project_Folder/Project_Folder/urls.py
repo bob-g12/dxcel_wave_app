88a31c5a53031abb2cd34a680f9771fe29fea128
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-27 20:16:00 +0900

    urls連結/App_folder表示

--- Project_Folder/Project_Folder/urls.py
+++ Project_Folder/Project_Folder/urls.py
@@ -1,26 +1,10 @@
-"""
-URL configuration for Project_Folder project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+# Include関数をインポート
+from django.urls import include
 
-# first_django_appフォルダ内のviews.pyをインポート
-from App_Folder import views as views
-
+# path関数でApp_Folder内のurls.pyを読み込むために、include関数を記述
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('views/',views.index,name='index'), # views記載の関数のpathを追記
+    path('admin/', admin.site.urls),                    # 管理画面のPath
+    path('App_Folder/', include('App_Folder.urls')),    # アプリケーション管理フォルダのurls.pyと関連づけるpath
 ]
