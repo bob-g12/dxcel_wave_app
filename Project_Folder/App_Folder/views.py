873747edcfc98daaae197c7b950432f78d7706a1
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-28 17:43:20 +0900

    Webページの表示設定

--- Project_Folder/App_Folder/views.py
+++ Project_Folder/App_Folder/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 # View関数を任意に定義
 def index(request):
-    return HttpResponse("Hello World")
+        # 変数設定
+    params = {"message_me": "Hello World"}
+    # 出力
+    return render(request,'App_Folder_HTML/index.html',context=params)
